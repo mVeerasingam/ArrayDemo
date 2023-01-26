fe5a8bb709eba8bc0c0c628dd5f681cddabc7c33
Mark Veerasingam <g00403931@atu.ie>
2023-01-26 11:43:51 +0000

    first commit

---
+++ src/main/java/ArrayDemo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+
+        int[] myNum = {100,200,300,400,500,600,700,800,900,1000};
+
+        System.out.println("Element at Index 0: " + myNum[0]);
+        System.out.println("Element at Index 1: " + myNum[1]);
+        System.out.println("Element at Index 2: " + myNum[2]);
+        System.out.println("Element at Index 3: " + myNum[3]);
+        System.out.println("Element at Index 4: " + myNum[4]);
+        System.out.println("Element at Index 5: " + myNum[5]);
+        System.out.println("Element at Index 6: " + myNum[6]);
+        System.out.println("Element at Index 7: " + myNum[7]);
+        System.out.println("Element at Index 8: " + myNum[8]);
+        System.out.println("Element at Index 9: " + myNum[9]);
+        System.out.println("Element at Index 10: " + myNum[10]);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number");
+        int inputNum = scanner.nextInt();
+    }
+}
